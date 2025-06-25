111729414f88e1a81529163ac0f88bb9f54463b0
Kostya <abc@d.com>
2025-06-25 11:33:49 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+print('Витязь на распутье')
+print("Налево (L) пойдешь, вольную-волю обретешь...")
+print("Направо (R) пойдешь, коня потеряешь...")
+print("Прямо (F), сыт и вел будешь...")
+choice = input("Куда идем (L, R или F): ")
+if choice == "L" or choice == "l":
+    print('Вольная воля')
+elif choice == "R" or choice == "r":
+    print('Конь сбежал')
+elif choice == "F" or choice == "f":
+    print('Сыт и весел')
+else:
+    print("Выбор не ясен")
+
+
