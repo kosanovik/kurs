a31b3f34a26da803033b4cfa8240409bfa8f97fb
Kostya <abc@d.com>
2025-06-26 14:57:18 +0300

    for loop begin

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 # for <переменная>. in iterable object:
 #    команды
 
-word = "поток"
+# word = "поток"
 
-for ch in word:
-    print(ch)
+# for ch in word:
+#    print(ch)
+#                   0       3   1
+# итератор range(start, stop, step)
+
+for i in range(0, 101, 5):
+# stop не включается
+    if i != 15:
+        print(i)
