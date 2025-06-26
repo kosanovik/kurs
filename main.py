b96732779eeb67c407a9e2155874ead4aeaf31b6
Kostya <abc@d.com>
2025-06-26 16:15:45 +0300

    Selection

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-# Вложенные циклы
+# Подбор по росту
+# 150 < height < 180
+# вводим до момента когда будет введен -1
+# число кандидатов, число прошедших по критерию, минимальное и максимальное значение по критерию
+from functools import total_ordering
 
-N = 5
-fact = 1
+total = 0
+total_success = 0
+total_unsuccess = 0
+min_val = float("inf")
+max_val = float("-inf")
 
-for i in range(1, 10):
-    for j in range (1, 10):
-        print(f"{i} * {j} = {i * j}", end="\t")
+while (num := int(input("Введите рост: "))) != -1:
+    if 150 <= num <= 180:
+        total_success += 1
+        if min_val > num:
+            min_val = num
+        if num > max_val:
+            max_val = num
+    total += 1
 
-print()
+print(f"Число кандидатов: {total}")
+print(f"Число прошедших отбор: {total_success}")
+print(f"Число минимального роста: {min_val}")
+print(f"Число максимального роста: {max_val}")
