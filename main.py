b29f76a7f7fc2e7a19d1aa91692415a8437e5f3c
Kostya <abc@d.com>
2025-06-26 12:47:55 +0300

    Loop Break

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+# break, continue
+
+
+
+
 num = 3 # число, которое надо угадать
 flag = True # флаг изменяет значение по событию
+var = ""
 
 print("Я загадал чило, угадай его!")
 
-while flag:
+while True:
     var = int(input("Ваше значение: "))
     if var == num:
         print("Ура, Угадал!")
-        flag = not flag # флаг инвертирован
-        # аналогично flag = False
+        break
     elif var > num:
         print("Число больше загаданного")
     else:
