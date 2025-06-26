0f62bd06b9f79b548aee08521b2eb4b0dbb7675e
Kostya <abc@d.com>
2025-06-26 15:49:45 +0300

    Factkrial _ 1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# factorial
+# Вложенные циклы
 
 N = 5
 fact = 1
 
-for i in range(1, N, + 1):
-    fact *= i
+for i in range(1, 10):
+    for j in range (1, 10):
+        print(f"{i} * {j} = {i * j}", end="\t")
 
-print(fact)
+print()
