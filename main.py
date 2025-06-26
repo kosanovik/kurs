3e4ba659d332c48f85ada2e8aabd8ec5ad5bf92b
Kostya <abc@d.com>
2025-06-26 13:02:12 +0300

    Loop Correct Higt _ 1

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # break, continue
-counter = 0
 
-while counter < 5:
-    counter += 1
-    if counter == 3:
-        continue
-    print(f"Итерация номер: {counter}")
+def get_height():
+    """
+    Запрашивает ввод роста, пока не будет введено корректное значение.
+    """
+    while True:
+        try:
+            height = float(input("Введите ваш рост в метрах: "))
+            if 0.5 < height < 2.5:  # Пример: рост должен быть между 0.5 и 2.5 метрами
+                return height
+            else:
+                print("Некорректный рост. Пожалуйста, введите значение в диапазоне от 0.5 до 2.5.")
+        except ValueError:
+            print("Некорректный ввод. Пожалуйста, введите число.")
+
+# Получение роста
+user_height = get_height()
+print(f"Ваш рост: {user_height} м")
