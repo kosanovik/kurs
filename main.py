b17b194430d23632624d750339c3502e6e938e31
Kostya <abc@d.com>
2025-06-27 15:42:52 +0300

    Encrypting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Cтроки (immutable, iterable) является не изменяемыми
 # Таблица символов
+from importlib.metadata import pass_none
 
 s = "\xB0"
 u = "\u2603"
@@ -15,3 +16,17 @@ print(chr(9731))
 print(chr(10000))
 print(chr(176)) # ASCII и Unicode
 
+s = set()
+word = input("Введите фразу для зашифровки: ")
+# Зашифровываем
+for ch in word:
+    s.add(ord(ch))
+
+print(s)
+
+# Расшифровываем
+
+res = ""
+for i in s:
+    res += chr(i)
+print(res)
