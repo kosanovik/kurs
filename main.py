71fa10683c851a96c9a43a7a759cdc5974360b44
Kostya <abc@d.com>
2025-06-30 14:30:04 +0300

    Ingredirnts list

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ lst = [] # пустой список
 while (item := input("Ингредиенты: ")) != "":
     lst.append(item)
 
-
+# исключаем повторы
 temp = set(lst)
 lst = list(temp)
 
+# набираем ингредиенты, сортируем
 print(f"У нас есть {len(lst)} ингредиентов: ")
-
 lst.sort()
 
+# выводим список
 for i in range(len(lst)):
-    print(f"{i + 1}. {lst[i]}")
+    print(f"\t{i + 1}. {lst[i]}")
 
 
