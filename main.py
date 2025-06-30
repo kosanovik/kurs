f6da5f6f014215638a874bd38b75439b1bf72007
Kostya <abc@d.com>
2025-06-30 10:42:05 +0300

    Start and End of string

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
 # Cтроки (immutable, iterable) является не изменяемыми
-from ctypes.wintypes import PWORD
+# Начало и окончание строки
+# startswith и endswith
 
-from pyexpat.errors import messages
+s = "Cмотреть"
 
-# s = "" - строка не изменяемый объект
-# s = set()
-# res = s.add()
-# s = set("") - итерированный объект преобразует в множество (изменяемый объект)
+if s.lower().startswith("смо"):
+    print("Да")
 
-# Создаем алфавит
-alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-# alphabet_u += alphabet.upper()
-
-
-# Получаем входные данные
-message = input("Введите строку: ").strip().lower() # обрубаем пробелы
-key = int(input("Введите ключ: "))
-
-# Инициализируем пустую строку для результата
-encrypted = ("")
-
-# Перебираем каждый символ
-for letter in message:
-    # Проверяем, является ли символ буквой из алфавита
-    if letter in alphabet.index(letter)
-    #
-    # Вычисляем новую позицию с учетом сдвига
-    new_key = (t + key) % len(alphabet)
-    # Добавляем зашифрованный символ
-    encrypted += alphabet[new_key]
-else:
-    # Если символ не буква, оставляем его без изменений
-    encrypted += letter
-
-# Для расшифровки достаточно изменит форму вычисления позиции:
-# new_key = (t - key) % len(alhabet)
-
-print("Зашифрованное сообщение: ",encrypted)
+if s.endswith("еть"):
+    print("Да")
