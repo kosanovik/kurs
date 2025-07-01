86b7357618a55cbd11c5a6fa7673816ae7a6414d
Kostya <abc@d.com>
2025-07-01 10:30:05 +0300

    Stop Words 2

--- main.py
+++ main.py
@@ -18,14 +18,19 @@ print(res)
 
 # Фраза: ну я типо вообще короче не понимаю этот язык
 
-
 stop_words = ['ну', 'типо', 'короче']
+
+
 temp = []
+
+
+stop_words = {'ну', 'типо', 'короче', 'не'}
 message = input('Введите сообщение: ')
 lst = message.split() # все слова
 for item in lst:
     if item not in stop_words:
         temp.append(item)
 res = sorted(temp)
+res = sorted(set(lst) - stop_words)
 for a, b in enumerate(res, 1):
     print(f'{a}. {b}')
