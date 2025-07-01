95087fba1eb396bae4e6d8e2c6bb8d5226b4ee8d
Kostya <abc@d.com>
2025-07-01 15:46:16 +0300

    Functions Intro

--- main.py
+++ main.py
@@ -1,104 +1,33 @@
-# Словари
-# Частотный анализ
+# Функции (Do not Repeat Yourself)
+# Scope (local or global(как можно меньше глобальных переменных))
+# Синтаксис
+# def <имя функции>([параметры]):
+#     команды
 
-res = {}
-
-text = "Министерство: труда и социальной защиты, разработало проект календаря праздничных и выходных дней на 2026-й год. "
-
-commas = (",", "-", ".", "!", ":")
-for x in commas:
-    text = text.replace(x, "")
-
-lst = text.strip().lower().split()
-print(lst)
-
-for item in lst:
-    if item in res.keys():
-        res[item] += 1
-    else:
-        res[item] = 1
-
-print("Частотный анализ слов текста")
-for k, v in  res.items():
-    print(f"\t{k}: {v}")
+# в функцию передается глобальная переменная
 
 
-# Пустой словарь
-# 1. d = {}
-# 2. d = dict()
-# Предзаполненый словарь
-
-d = {
-    "table": ["таблица", "стол"],
-    "well": ["хорошо", "колодец"],
-    "chair": "стул",
-    "apple": "яблоко",
-    1: "один",
-    (55.75, 37.5): "Москва"
-}
-
-print(d[(55.75, 37.5)])
-
-print(d["table"])
-print(d["well"][0])
-
-if type(d["well"]) == list:
-    d["well"].append("скважина")
-
-print(d[1])
-
-d["plum"] = "слива"
-d["well"].append("скважина")
-print(d["plum"])
-del d["well"]
-
-# print(d) # - словарь целиком как есть
-
-deleted_item = d.pop("apple")
-print("Удалился элемент:", deleted_item)
-
-print("Если стул в словаре")
-if "стул" in d.values():
-    print("Да есть")
-
-print("доступ к несуществующему ключу без исключений")
-pear = d.get("pear", "Груши нет") # мягкое обращение по ключу
-print("Где груша: ", pear)
-
-# перебор по умолчанию
-for key in d:
-    print(key, "->", d[key])
-
-# перебор пар "ключ-значение"
-for k, v in d.items():
-    print(k, "->", v)
-
-print(d.keys()) # список ключей (list)
-print(list(d.keys())) # - список
-print(d.values()) # список значений
-print(d.items()) # список пар (ключ-значения)
-
-# методы словаря
-# 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 'setdefault', 'update', 'values'
+person = "Пётр" # глобальная
+count = 0
 
 
-# N = 3
-# matrix = [[i + j for j in range(N)]for i in range(1, 10, 3)]
-# print(matrix)
+def greet_to_name(name="noname"):
+    print("Привет,", name)
+    print(count)
 
 
-# matrix = []
-# table = []
-#
-# start = 1
-# N = 4
-#
-# for i in range(N):
-#     for j in range(start, start + N):
-#         table.append(j)
-#     matrix.append(table)
-#     table = []
-#     start += N
-#
-# print(matrix)
+def increment():
+    global count
+    count += 1
 
+
+def print_list(array=None):
+    if array is None:
+        array = []
+    for item in array:
+        print(item)
+
+
+increment()
+greet_to_name("name")
+print_list()
