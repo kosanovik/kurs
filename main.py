efe1499db28f95df5c6fa6fa5a9bc13a6cd7ff08
Kostya <abc@d.com>
2025-07-01 13:01:01 +0300

    Dictionary Intro

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
-# Вложенные списки
-N = 3
-matrix = [[i + j for j in range(N)]for i in range(1, 10, 3)]
-print(matrix)
+# Словари
+# Пустой словарь
+# 1. d = {}
+# 2. d = dict()
+# Предзаполненый словарь
+
+d = {
+    "table": ["таблица", "стол"],
+    "well": ["хорошо", "колодец"],
+    "chair": "стул",
+    "apple": "яблоко",
+    1: "один",
+}
+
+print(d["table"])
+print(d["well"][0])
+
+if type(d["well"]) == list:
+    d["well"].append("скважина")
+
+print(d[1])
+
+d["plum"] = "слива"
+d["well"].append("скважина")
+print(d["plum"])
+del d["well"]
+
+# print(d) # - словарь целиком как есть
+
+deleted_item = d.pop("apple")
+print("Удалился элемент:", deleted_item)
+
+print("Если стул в словаре")
+if "chair" in d:
+    print("Да есть")
+
+for key in d:
+    print(key, "->", d[key])
+
+# методы словаря
+# 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 'setdefault', 'update', 'values'
+
+
+# N = 3
+# matrix = [[i + j for j in range(N)]for i in range(1, 10, 3)]
+# print(matrix)
 
 
 # matrix = []
