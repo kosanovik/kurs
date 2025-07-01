87017f39206a8eabd6aac301704d017268792144
Kostya <abc@d.com>
2025-07-01 12:17:18 +0300

    Nested lists _ 2 _ generation

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 # Вложенные списки
-from itertools import count
 
-# a = [1, 38.6, True, False, "sfs", (1, 2)] a - array
-N = 3
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-]
+matrix = []
+table = []
 
-matrix = [[1] * N for _ in range(N)] # _ - переменная кот не используется нигде
-print(matrix)
-# обход 2-мерного списка (матрицы)
-count = 1
-for row in range(len(matrix)):
-    for col in range(len(matrix[row])):
-        matrix[row][col] = count
-        count += 1
+start = 1
+N = 4
+
+for i in range(N):
+    for j in range(start, start + N):
+        table.append(j)
+    matrix.append(table)
+    table = []
+    start += N
+
 print(matrix)
 
