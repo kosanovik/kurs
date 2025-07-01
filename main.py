769bce1d648a4b124eb6440b7f557f79f3c2e722
Kostya <abc@d.com>
2025-07-01 12:06:07 +0300

    Nested lists

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-# Спичочные выражения (list comprehension)
-# Зансти в список каждое третье слово из предложения
-text = "Списочные выражения применяются для эффективности кода"
+# Вложенные списки
 
-res = [a for a in text.split() if (text.index(a) + 1) % 3 == 0] # не корректно
-print(res)
-res = [a for a in text.split()[2::3]] # операции со списком
-print(res)
-res = set(a for a in text.split()[2::3])
-print(res)
+# a = [1, 38.6, True, False, "sfs", (1, 2)] a - array
+N = 3
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+matrix = [[1] * N for _ in range(N)] # _ - переменная кот не используется нигде
+print(matrix)
+# обход 2-мерного списка (матрицы)
+for row in range(len(matrix)):
+    for col in range(len(matrix[row])):
+        print(matrix[row][col])
+# print(matrix)
+
