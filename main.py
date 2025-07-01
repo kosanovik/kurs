5f9813240efdf583b00388965942635b46841394
Kostya <abc@d.com>
2025-07-01 10:32:44 +0300

    Stop Words 3

--- main.py
+++ main.py
@@ -13,24 +13,16 @@ print(res)
 # слова из стоп листа не попадают в список. список номерованный
 # вводится текст, стоп лист и получившийся список слов (сортированный по алфавиту без повторов) - стоп_слова убрать в конце
 
-# ДЗAdd commentMore actions
+# ДЗ
 
-
-# Фраза: ну я типо вообще короче не понимаю этот язык
-
-stop_words = ['ну', 'типо', 'короче']
-
-
-temp = []
-
-
+# Фраза: ну?, я типо, вообще: короче, не понимаю этот язык!
+commas = (',', '!', '.', '?', '-', ':')
 stop_words = {'ну', 'типо', 'короче', 'не'}
 message = input('Введите сообщение: ')
 lst = message.split() # все слова
-for item in lst:
-    if item not in stop_words:
-        temp.append(item)
-res = sorted(temp)
+for z in commas:
+    message = message.replace(z, '')
+lst = message.split()  # все слова
 res = sorted(set(lst) - stop_words)
 for a, b in enumerate(res, 1):
     print(f'{a}. {b}')
