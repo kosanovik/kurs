4c642bdb405cbc3f1072650523d1dcea4e4cefad
Kostya <abc@d.com>
2025-07-03 11:37:09 +0300

    dict comprehense

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # Анонимные функции (однострочники, безымянные)
 # lambda-функции
 # lambda <аргументы>: <выражение>
+# словарные аргументы
+
+numbers = [1, 2, 3, 4, 5] # list(range(1, 6))
+squares = {n: n ** 2 for n in numbers}
+print(squares)
+
+numbers = range(1, 11)
+squares = {n: n ** 2 for n in numbers if n % 2 == 0}
+print(squares)
+
+source_dict = {
+    "x": 1,
+    "y": 2,
+    "z": 3,
+}
+
+dest_dict = {k: v * 2 for k, v in source_dict.items()}
+print(dest_dict)
+
 
 ENGLISH_ABC = set([chr(ch) for ch in range(ord("a"), ord("z") + 1)])
 RUSSIAN_ABC = set([chr(ch) for ch in range(ord("а"), ord("я") + 1)] + ["ё"])
 # print(ENGLISH_ABC)
 # print(RUSSIAN_ABC)
-ABC = ENGLISH_ABC ^ RUSSIAN_ABC ^ set(map(str.upper, ENGLISH_ABC))
+# ABC = ENGLISH_ABC ^ RUSSIAN_ABC ^ set(map(str.upper, ENGLISH_ABC))
 ABC = ENGLISH_ABC ^ RUSSIAN_ABC ^ set([x.upper() for x in ENGLISH_ABC]) ^ set([x.upper() for x in RUSSIAN_ABC])
 print(ABC)
 
