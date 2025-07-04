1634dea7a3d9ea22435af331e5c407f9722853de
Kostya <abc@d.com>
2025-07-04 10:15:40 +0300

    Home Task Solution

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 # Черепашья графика
 
+import sys
+from operator import length_hint
+
+strings = [d.strip("\n") for d in sys.stdin.readlines()]
+length = len(strings) # сколько строк
+rem = length % 3
+
+if rem:
+    strings = strings[:length - rem]
+
+for x in range(0, length - rem, 3):
+    summ = sum(len(a) for a in strings[x:x + 3])
+    result = []
+    for s in strings[x:x + 3]:
+        temp = s.lower().split()
+        result += filter(lambda a: len(a) % 2 == summ % 2, temp)
+    result = sorted(set(map(lambda b: b.capitalize(), result)))[:5]
+    print(*result, sep=". ")
+
+
+
+
+
 import turtle as t # псевдоним
 
 N = 50
@@ -51,6 +74,7 @@ def tree(lenght): # рекурсивное дереао
 
 t.left(90)
 tree(100)
+
 
 
 # for _ in range(N):
