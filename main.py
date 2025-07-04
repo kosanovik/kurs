7960445b532e011981be1b56f18b44c13d2292ac
Kostya <abc@d.com>
2025-07-04 10:32:56 +0300

    min, max, sum

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
+# Встроенные функции
+
+
+
+lst = [1, 1, 2, 3, 5]
+res = sum([1, 2, 3])
+print(res)
+min_value = min(lst)
+max_value = max(lst)
+print(res, min_value, max_value)
+
+
+
+
 # Черепашья графика
 
 import sys
 from operator import length_hint
 
-strings = [d.strip("\n") for d in sys.stdin.readlines()]
-length = len(strings) # сколько строк
-rem = length % 3
-
-if rem:
-    strings = strings[:length - rem]
-
-for x in range(0, length - rem, 3):
-    summ = sum(len(a) for a in strings[x:x + 3])
-    result = []
-    for s in strings[x:x + 3]:
-        temp = s.lower().split()
-        result += filter(lambda a: len(a) % 2 == summ % 2, temp)
-    result = sorted(set(map(lambda b: b.capitalize(), result)))[:5]
-    print(*result, sep=". ")
+# strings = [d.strip("\n") for d in sys.stdin.readlines()]
+# length = len(strings) # сколько строк
+# rem = length % 3
+#
+# if rem:
+#     strings = strings[:length - rem]
+#
+# for x in range(0, length - rem, 3):
+#     summ = sum(len(a) for a in strings[x:x + 3])
+#     result = []
+#     for s in strings[x:x + 3]:
+#         temp = s.lower().split()
+#         result += filter(lambda a: len(a) % 2 == summ % 2, temp)
+#     result = sorted(set(map(lambda b: b.capitalize(), result)))[:5]
+#     print(*result, sep=". ")
 
 
 
