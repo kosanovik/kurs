608bc57a66a351e918c6217a65e96111f385527b
Kostya <abc@d.com>
2025-07-07 10:28:34 +0300

    Convert and Filters

--- main.py
+++ main.py
@@ -8,33 +8,47 @@
 
 from PIL import Image, ImageDraw, ImageFont
 
-# https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru
-W = 600
-H = 400
+orig = Image.open("images/sunny_day.jpg").convert("RGB")
 
-image = Image.new('RGB',
-                  (W, H),
-                  (0, 163, 232))
+up = orig.crop((0, 0, 600, 200))
+down = orig.crop((0, 200, 600, 400))
 
-draw = ImageDraw.Draw(image)
+new = Image.new("RGB", (600, 400))
 
-text = 'Солнечный день'
-# draw.ellipse((470, -120, 800, 120), outline='yellow', fill='yellow')
-draw.circle((600, 0), 100, fill='yellow')
-font = ImageFont.truetype(
-    font='fonts/Geisha.ttf',  # можно использовать любой установленный шрифт
-    size=50
-)
-# Получаем размеры текста
-_, _, w, h = draw.textbbox((0, 0), text, font=font)
+new.paste(down, (0, 0))
+new.paste(up, (0, 200))
 
-# Рассчитываем позицию для центрирования
-x = (W - w) // 2
-y = (H - h) // 2
+new.show()
 
-draw.text((x, y), text, fill=(255, 255, 0), font=font)
-
-image.show()
+# from PIL import Image, ImageDraw, ImageFont
+#
+# # https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru
+# W = 600
+# H = 400
+#
+# image = Image.new('RGB',
+#                   (W, H),
+#                   (0, 163, 232))
+#
+# draw = ImageDraw.Draw(image)
+#
+# text = 'Солнечный день'
+# # draw.ellipse((470, -120, 800, 120), outline='yellow', fill='yellow')
+# draw.circle((600, 0), 100, fill='yellow')
+# font = ImageFont.truetype(
+#     font='fonts/Geisha.ttf',  # можно использовать любой установленный шрифт
+#     size=50
+# )
+# # Получаем размеры текста
+# _, _, w, h = draw.textbbox((0, 0), text, font=font)
+#
+# # Рассчитываем позицию для центрирования
+# x = (W - w) // 2
+# y = (H - h) // 2
+#
+# draw.text((x, y), text, fill=(255, 255, 0), font=font)
+#
+# image.show()
 # image.save('images/sunny_day.jpg')
 
 # from PIL import Image, ImageDraw # Image - главный составной компонет библиотеки
