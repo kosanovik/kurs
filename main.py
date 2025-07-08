22248568df8a73976c9f97bf417e52dc9b9249fa
Kostya <abc@d.com>
2025-07-08 15:01:25 +0300

    PyMorphy

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+# Библиотека pymorphy
+# pip install pymorphy3
+# pip install -U pymorphy3-dicts-ru
+
+from pymorphy3 import MorphAnalyzer
+
+form = MorphAnalyzer().parse('бутылка')[0]
+
+for btl in reversed(range(99)):
+    print(f'В холодильнике {btl + 1} {form.make_agree_with_number(btl + 1).word} пива')
+    print('Возьмём одну и выпьем')
+    if btl % 10 == 1 and btl != 11:
+        remain = 'Осталась'
+    else:
+        remain = 'Осталось'
+    print(f'{remain} {btl} {form.make_agree_with_number(btl).word} пива.')
+
+
 # Исключения (runtime - время исполнения программы)
 # try:
 #   что пытаемся сделать
@@ -26,8 +44,6 @@
 #         fo.write('По умолчанию')
 #         fo.close()
 #         print('Продолжаем работать.')
-from fnmatch import translate
-from sys import excepthook
 
 # Задача 1
 # lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -45,58 +61,58 @@ from sys import excepthook
 
 # Практикум (обучаемый словарь)
 
-import pickle
-
-# минимальная версия, если файл dict.dat отсутствует
-voc = {
-    'стол': 'table',
-    'стул': 'chair',
-}
-
-
-# функция для распечатки словаря
-def print_voc():
-    print('Сейчас словарь содержит: ')
-    for k, v in voc.items():
-        print(k, '—', v)  # Alt + 0151
-
-# загружаем словарь из файла
-try:
-    with open('dict.dat', 'rb') as dump_in:
-        voc = pickle.load(dump_in)
-except FileNotFoundError:
-    with open('dict.dat', 'wb') as dump_out:
-        pickle.dump(voc, dump_out)
-    print('Создан минимальный словарь: ')
-    print_voc()
-
-while True:
-    temp = input('\nВведите слово для перевода или "#" для завершения: ')
-    word = temp.strip().lower()
-    if word == '#' or word == '№':
-        break
-    if word in voc.keys():
-        translate = voc[word]
-        print(f'Слово "{word}" переводится как {translate}.\n')
-    else:
-        print(f'Значение слова {word} отсутствует в словаре.')
-        newkey = f'А как слово {word} переводится.\n'
-        newkey += 'Если ничего не вводите нажмите ENTER,\n '
-        newkey += 'или введите его здесь: '
-        new_word = input(newkey)
-
-        if new_word != '' or len(new_word) > 2:
-            voc[word] = new_word
-            print(f'Слово {word} с переводом {new_word} внесено в словарь')
-        else:
-            print('Ничего не введено или слишком короткое слово')
-            continue
-
-# Сохранить словарь
-with open('dict.dat', 'wb') as dump_out:
-    pickle.dump(voc, dump_out)
-
-print('До новых встреч!!!')
+# import pickle
+#
+# # минимальная версия, если файл dict.dat отсутствует
+# voc = {
+#     'стол': 'table',
+#     'стул': 'chair',
+# }
+#
+#
+# # функция для распечатки словаря
+# def print_voc():
+#     print('Сейчас словарь содержит: ')
+#     for k, v in voc.items():
+#         print(k, '—', v)  # Alt + 0151
+#
+# # загружаем словарь из файла
+# try:
+#     with open('dict.dat', 'rb') as dump_in:
+#         voc = pickle.load(dump_in)
+# except FileNotFoundError:
+#     with open('dict.dat', 'wb') as dump_out:
+#         pickle.dump(voc, dump_out)
+#     print('Создан минимальный словарь: ')
+#     print_voc()
+#
+# while True:
+#     temp = input('\nВведите слово для перевода или "#" для завершения: ')
+#     word = temp.strip().lower()
+#     if word == '#' or word == '№':
+#         break
+#     if word in voc.keys():
+#         translate = voc[word]
+#         print(f'Слово "{word}" переводится как {translate}.\n')
+#     else:
+#         print(f'Значение слова {word} отсутствует в словаре.')
+#         newkey = f'А как слово {word} переводится.\n'
+#         newkey += 'Если ничего не вводите нажмите ENTER,\n '
+#         newkey += 'или введите его здесь: '
+#         new_word = input(newkey)
+#
+#         if new_word != '' or len(new_word) > 2:
+#             voc[word] = new_word
+#             print(f'Слово {word} с переводом {new_word} внесено в словарь')
+#         else:
+#             print('Ничего не введено или слишком короткое слово')
+#             continue
+#
+# # Сохранить словарь
+# with open('dict.dat', 'wb') as dump_out:
+#     pickle.dump(voc, dump_out)
+#
+# print('До новых встреч!!!')
 
 # import pickle
 #
@@ -765,9 +781,6 @@ print('До новых встреч!!!')
 
 
 # Черепашья графика
-
-import sys
-from operator import length_hint, index
 
 # strings = [d.strip("\n") for d in sys.stdin.readlines()]
 # length = len(strings) # сколько строк
