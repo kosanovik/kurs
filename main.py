14f0a24ab430b35a5cdbc0137354ded8b9865e3a
Kostya <abc@d.com>
2025-07-09 11:44:14 +0300

    ReExp search in site

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import requests
 pattern = r'<img[^>]+src="([^">]+)"'
 # Сначала проверили
 # test_string = '<img height="50" width="150" src="images/bg.jpg">'
-html = requests.get('https://skilbox.ru').text
+html = requests.get('https://skillbox.ru').text
 result = re.findall(pattern, html)
 print(result)
 
