09647c50bd5d93cfcf12b5385cae759fe94a97cb
Kostya <abc@d.com>
2025-07-10 15:08:50 +0300

    Inheritans Intro

--- main.py
+++ main.py
@@ -1,22 +1,87 @@
-# ООП (magic methods)
-#
+# ООП (inheritance)
+# класс, от которого наследуем: базовый, родительский, суперкласс
+# класс, который наследуется называется производным и дочерним
+
+class Circle:
+
+    def __init__(self, radius):
+        self.radius = radius
+        self.name = 'круг'
+
+    def perimetr(self):
+        return round(2 * pi * self.radius, 2)
+
+    def area(self):
+        return round(pi * self.radius ** 2, 2)
+
+    def get_name(self):
+        return self.name
+
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.name = 'прямоугольник'
+
+    def perimetr(self):
+        return 2 * (self.width + self.height)
+
+    def area(self):
+        return self.width * self.height
+
+    def get_name(self):
+        return self.name
+
+
+class Square(Rectangle):
+
+    def __init__(self, side):
+        super().__init__(side, side)
+        self.side = side
+        self.name = 'квадрат'
+
+    # def perimetr(self):
+    #     return 4 * self.side
+    #
+    # def area(self):
+    #     return self.side ** 2
+
+    def get_name(self):
+        return self.name
+
+s = Square(5)
+print(s.area())
+print(s.perimetr())
+print(s.get_name())
+
+
+
+
+
+
+
+
+
+
+
 # __call__ - экземпляр класса становиться вызываемым
 # (как функция)
 # y = ax^2 + bx + c
 
 
-class SquareFunction:
-
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-
-    def __call__(self, x):
-        return self.a * x ** 2 + self.b * x + self.c
-
-s = SquareFunction(1, 2, 3)
-print(s(2))
+# class SquareFunction:
+#
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#
+#     def __call__(self, x):
+#         return self.a * x ** 2 + self.b * x + self.c
+#
+# s = SquareFunction(1, 2, 3)
+# print(s(2))
 
 
 
