78e9daebedb23bbd31fa2b5322f3d40cb7cde5a8
Kostya <abc@d.com>
2025-07-10 14:48:48 +0300

    Special (Magic) Methods Call

--- main.py
+++ main.py
@@ -1,41 +1,60 @@
 # ООП (magic methods)
 #
-
-class Mytime:
-    def __init__(self, minutes, seconds):
-            self.seconds = seconds
-            self.minutes = minutes
-
-    def __add__(self, other):
-        m = self.minutes + other.minutes
-        s = self.seconds + other.seconds
-        m += s // 60
-        s = s % 60
-        m = m % 60
-        return Mytime(m, s)
-
-    def __str__(self):
-        return f"<Time {self.minutes:02}:{self.seconds:02}>"
+# __call__ - экземпляр класса становиться вызываемым
+# (как функция)
+# y = ax^2 + bx + c
 
 
-t1 = Mytime(13, 15)
-t2 = Mytime(43, 25)
-print(t1 + t2)
+class SquareFunction:
 
-class Mytime:
-    def __init__(self, minutes, seconds):
-        if 0 <= minutes < 60:
-            self.minutes = minutes
-        if 0 <=seconds < 60:
-            self.seconds = seconds
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
-    def __str__(self):
-        return f"<Time {self.minutes}:{self.seconds}>"
+    def __call__(self, x):
+        return self.a * x ** 2 + self.b * x + self.c
+
+s = SquareFunction(1, 2, 3)
+print(s(2))
 
 
-t1 = Mytime(13, 15)
-# t2 = Mytime(43, 25)
-print(t1)
+
+# class Mytime:
+#     def __init__(self, minutes, seconds):
+#             self.seconds = seconds
+#             self.minutes = minutes
+#
+#     def __add__(self, other):
+#         m = self.minutes + other.minutes
+#         s = self.seconds + other.seconds
+#         m += s // 60
+#         s = s % 60
+#         m = m % 60
+#         return Mytime(m, s)
+#
+#     def __str__(self):
+#         return f"<Time {self.minutes:02}:{self.seconds:02}>"
+#
+#
+# t1 = Mytime(13, 15)
+# t2 = Mytime(48, 25)
+# print(t1 + t2)
+#
+# class Mytime:
+#     def __init__(self, minutes, seconds):
+#         if 0 <= minutes < 60:
+#             self.minutes = minutes
+#         if 0 <=seconds < 60:
+#             self.seconds = seconds
+#
+#     def __str__(self):
+#         return f"<Time {self.minutes}:{self.seconds}>"
+#
+#
+# t1 = Mytime(13, 15)
+# # t2 = Mytime(43, 25)
+# print(t1)
 
 
 
