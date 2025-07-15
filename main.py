0e3ff990c2860ba4c69124c8e7923becf559da18
Kostya <abc@d.com>
2025-07-15 14:19:57 +0300

    SQL Class CRUD

--- main.py
+++ main.py
@@ -7,29 +7,82 @@
 5. Подтвердить изменение (commit)
 6. Отключаемся от БД
 """
+
 import sqlite3
-import csv
 
-# Подключаемся
-connection = sqlite3.connect('db/movies.sqlite')
 
-# Курсор
-cursor = connection.cursor()
+class Crud:
+    def __init__(self, db_path):
+        self._conn = sqlite3.connect(db_path)
+        self._cur = self._conn.cursor()
 
-# Запрос (с помощью курсора)
-with open('people.csv', 'r', encoding='utf-8') as f:
-    reader = csv.reader(f, delimiter=',')
-    next(reader)  # пропустить заголовок (первая строка)
-    for name, age in reader:
-        cursor.execute(
-            """
-            INSERT INTO users(name, age)
-            VALUES(?, ?)
-            """, (name, int(age))
+    def create(self, table_name, name, age):
+        self._cur.execute(
+            f"""
+            INSERT INTO {table_name}(name, age)
+             VALUES(?, ?)
+             """, (name, int(age))
         )
+        self._conn.commit()
 
-connection.commit()  # Подтверждение
-connection.close()  # Закрываем подключени
+    def read(self, table_name):
+        res = self._cur.execute(
+            f'SELECT * FROM {table_name}'
+        ).fetchall()
+
+        for num, name, age in res:
+            print(num, name, age)
+
+    def update(self, table_name, id_num, name=None, age=None):
+        query = f'UPDATE {table_name} SET name="{name}", age={age} WHERE id={id_num}'
+        # print(query)
+        self._cur.execute(
+            query
+        )
+        self._conn.commit()
+
+    def delete(self, id_num, table_name):
+        self._cur.execute(
+            f'DELETE FROM {table_name} WHERE id={id_num}'
+        )
+        self._conn.commit()
+
+    # method override (переопределяем метод уничтожения объекта)
+    def __del__(self):
+        self._cur.close()  # отключаем курсор
+        self._conn.close()  # отключаемся от БД
+
+
+db = Crud('db/movies.sqlite')
+# db.delete(3, 'users')
+# db.create('users', 'Дмитрий', 18)
+db.update('users', 8, 'Евгений', 19)
+db.read('users')
+
+
+# import sqlite3
+# import csv
+#
+# # Подключаемся
+# connection = sqlite3.connect('db/movies.sqlite')
+#
+# # Курсор
+# cursor = connection.cursor()
+#
+# # Запрос (с помощью курсора)
+# with open('people.csv', 'r', encoding='utf-8') as f:
+#     reader = csv.reader(f, delimiter=',')
+#     next(reader)  # пропустить заголовок (первая строка)
+#     for name, age in reader:
+#         cursor.execute(
+#             """
+#             INSERT INTO users(name, age)
+#             VALUES(?, ?)
+#             """, (name, int(age))
+#         )
+#
+# connection.commit()  # Подтверждение
+# connection.close()  # Закрываем подключени
 # fetchall - всё
 # fetchone - только первое соответствие
 # fetchmany(N) - N - соответствий
