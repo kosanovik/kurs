fcb4e789f6dc4bbfb5f12b67c7a105b52449974e
Kostya <abc@d.com>
2025-07-15 15:39:38 +0300

    Function Def in other Func

--- main.py
+++ main.py
@@ -1,42 +1,63 @@
-# Погода через API
-from http.client import responses
-
-import requests
-from PIL import Image
-import io
-
-API_KEY = "c747bf84924be997ff13ac5034fa3f86"
-URL = 'http://api.openweathermap.org/data/2.5/weather'
-CITY = "Санкт-Петербург"
-
-params = {
-    "q": CITY,
-    "appid": API_KEY,
-    "units": "metric",
-    "lang": "ru"
-}
-
-response = requests.get(URL, params=params)
-
-result = response.json()
-weather = result["weather"][0]["description"]
-temperature = result['main']["temp"]
-humidity = result["main"]["humidity"]
-wind = result["wind"]["speed"]
-data = result["coord"]
-ll = f"{data["lon"]},{data["lat"]}"
-# print(ll)
+# Декораторы
+def answer(question):
+    return 'думайте сами'
 
 
-print(f"Сегодня в городе {CITY}: {weather}")
-print(f'Температура: {temperature:.1f}\xB0C')
-print(f"Влажность: {humidity}%")
-print(f"Скорость ветра: {wind} м/с")
-link = f'https://static-maps.yandex.ru/1.x/?ll={ll}&spn=0.005,0.005&l=map&pt={ll},pm2dgl'
-image = requests.get(link).content
-if image:
-    im = Image.open(io.BytesIO(image)).convert("RGB")
-    im.save('map.jpg')
+def dialog():
+    def answer(question):
+        if question.lower().startswith('когда'):
+            return 'Никогда'
+        else:
+            return 'Уппппс'
+
+    question = input()
+    while question != '':
+        print(answer(question))
+        question = input()
+
+
+dialog()
+
+
+# # Погода через API
+# from http.client import responses
+#
+# import requests
+# from PIL import Image
+# import io
+#
+# API_KEY = "c747bf84924be997ff13ac5034fa3f86"
+# URL = 'http://api.openweathermap.org/data/2.5/weather'
+# CITY = "Санкт-Петербург"
+#
+# params = {
+#     "q": CITY,
+#     "appid": API_KEY,
+#     "units": "metric",
+#     "lang": "ru"
+# }
+#
+# response = requests.get(URL, params=params)
+#
+# result = response.json()
+# weather = result["weather"][0]["description"]
+# temperature = result['main']["temp"]
+# humidity = result["main"]["humidity"]
+# wind = result["wind"]["speed"]
+# data = result["coord"]
+# ll = f"{data["lon"]},{data["lat"]}"
+# # print(ll)
+#
+#
+# print(f"Сегодня в городе {CITY}: {weather}")
+# print(f'Температура: {temperature:.1f}\xB0C')
+# print(f"Влажность: {humidity}%")
+# print(f"Скорость ветра: {wind} м/с")
+# link = f'https://static-maps.yandex.ru/1.x/?ll={ll}&spn=0.005,0.005&l=map&pt={ll},pm2dgl'
+# image = requests.get(link).content
+# if image:
+#     im = Image.open(io.BytesIO(image)).convert("RGB")
+#     im.save('map.jpg')
 
 # Базы данных (запись)
 # """
