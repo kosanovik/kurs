79061ec31e6e515e4ef8dc826a53abf14fedb83a
Kostya <abc@d.com>
2025-07-16 11:48:23 +0300

    Sample Page HTML

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # Введение во Flask
 # MVC-(Model View Controller)
 #
-from flask import Flask
+from fileinput import filename
+from tkinter import image_names
+
+from flask import Flask, url_for
+
 
 app = Flask(__name__)
-
+debug = False
 
 @app.route("/")
 @app.route("/index")
@@ -18,6 +22,7 @@ def about():
     print("Вызвана функция about")
     return "О нас"
 
+
 @app.route("/countdown")
 def cd():
     lst = [str(x) for x in reversed(range(10))]
@@ -25,5 +30,25 @@ def cd():
     print("Вызвана функция countdown")
     return "<br>".join(lst)
 
+
+@app.route("/image")
+def show_image():
+#    return '<img src="./static/images/python.jpg">'
+    return f'<img src="{url_for('static', filename='images/python2.jpg')}">'
+
+
+@app.route('/sample-page')
+def sample_page():
+    return f"""<!DOCTYPE html>
+            <html lang="ru">
+                <head><meta charset="UTF-8">
+                <title>Картинка Питона</title>
+            </head>
+            <body>
+                <img src="{url_for('static', filename='images/python2.jpg')}" alt="Python">
+            </body>
+            </html>
+    """
+
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True) # localhost - 127.0.0.1
+    app.run(host="localhost", port=5000, debug=debug) # localhost - 127.0.0.1
