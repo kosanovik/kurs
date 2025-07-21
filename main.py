1b5faa80593392aa57b4f5dd301252f596586a1c
Kostya <abc@d.com>
2025-07-21 12:21:56 +0300

    Login Success

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from data.news import News
 from data.users import User
 from forms.loginform import LoginForm
 from forms.user import Register
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
 
@@ -76,7 +76,15 @@ def contacts():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return 'Форма отправлена'
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login.html',
+                               message='Неверный логин или пароль',
+                               title='Ошибка авторизации',
+                               form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
