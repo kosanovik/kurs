bb57f01119da08ba4d598573d41bea6d3d6003b8
Kostya <abc@d.com>
2025-07-22 11:07:57 +0300

    Add News Api

--- data/news_api.py
+++ data/news_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 
 from . import db_session
 from .news import News
@@ -35,3 +35,22 @@ def get_one_news(news_id):
             'news': news.to_dict(only=('title', 'content', 'user_id', 'is_private'))
         }
     )
+
+
+@blueprint.route('/api/news', methods=['POST'])
+def create_news():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['title', 'content', 'user_id', 'is_private']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    news = News(
+        title=request.json['title'],
+        content=request.json['content'],
+        user_id=request.json['user_id'],
+        is_private=request.json['is_private']
+    )
+    db_sess.add(news)
+    db_sess.commit()
+    return jsonify({'id': news.id})
