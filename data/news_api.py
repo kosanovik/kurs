e87027f0676c10bab4e423128c70d968477f2984
Kostya <abc@d.com>
2025-07-22 11:46:05 +0300

    Delete News Api

--- data/news_api.py
+++ data/news_api.py
@@ -54,3 +54,14 @@ def create_news():
     db_sess.add(news)
     db_sess.commit()
     return jsonify({'id': news.id})
+
+
+@blueprint.route('/api/news/<int:news_id>', methods=['DELETE'])
+def delete_news(news_id):
+    db_sess = db_session.create_session()
+    news = db_sess.get(News, news_id)
+    if not news:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    db_sess.delete(news)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
